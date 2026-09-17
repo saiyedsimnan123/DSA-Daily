ec90efce444b021f13be3f4d1418a464de55f7a2
saiyedsimnan123 <saiyedsimnan95@gmail.com>
2026-09-17 18:31:54 +0530

    Implement reverse linked list function

---
+++ Day-16/ReverseLinkedList.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseList(self, head):
+        previous = None
+        current = head
+
+        while current:
+            next_node = current.next
+            current.next = previous
+
+            previous = current
+            current = next_node
+
+        return previous
